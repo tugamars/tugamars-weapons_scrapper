91e1f9851c85457ca4651a9e2718ddde7ea41e3d
tugamars <mars.silva.13@gmail.com>
2024-10-22 05:19:38 +0100

    Added new image output formats

--- main/server/screenshoter/server.js
+++ main/server/screenshoter/server.js
@@ -1,6 +1,29 @@
 /// <reference types="@citizenfx/server" />
 /// <reference types="image-js" />
 
+function beforeLast (value, delimiter) {
+	value = value || ''
+
+	if (delimiter === '') {
+		return value
+	}
+
+	const substrings = value.split(delimiter)
+
+	return substrings.length === 1
+		? value // delimiter is not part of the string
+		: substrings.slice(0, -1).join(delimiter)
+}
+function afterLast (value, delimiter) {
+	value = value || ''
+
+	return delimiter === ''
+		? value
+		: value.split(delimiter).pop()
+}
+
+
+
 const imagejs = require('image-js');
 const config = JSON.parse(LoadResourceFile(GetCurrentResourceName(), 'main/shared/screenshoter/config.json'));
 
@@ -8,6 +31,59 @@ let weaponImages=[];
 let componentsImages=[];
 
 if(config.enable){
+
+	async function trimTransparentBorders(imagePath, outputPath) {
+		// Load the image
+		const image = await imagejs.Image.load(imagePath);
+
+		// Get image dimensions
+
+		// Find the boundaries of non-transparent pixels
+		let top = null, left = null, right = null, bottom = null;
+
+		for (let x = 0; x < image.width; x++) {
+			for (let y = 0; y < image.height; y++) {
+				const pixelArr = image.getPixelXY(x, y);
+				const r = pixelArr[0];
+				const g = pixelArr[1];
+				const b = pixelArr[2];
+
+				if (g > r + b) {
+					image.setPixelXY(x, y, [255, 255, 255, 0]);
+				}
+			}
+		}
+
+		for (let y = 0; y <  image.height; y++) {
+			for (let x = 0; x < image.width; x++) {
+				const alpha = image.getPixelXY(x, y)[3]; // Get the alpha channel (4th value in RGBA)
+
+				if (alpha > 0) { // If pixel is not transparent
+					if (top === null) top = y;
+					if (left === null || x < left) left = x;
+					if (right === null || x > right) right = x;
+					bottom = y;
+				}
+			}
+		}
+
+		// If the image is fully transparent, we don't need to crop
+		if (top === null) {
+			console.log("Image is fully transparent. No cropping applied.");
+			return;
+		}
+
+		// Define the cropped area
+		const croppedWidth = right - left + 1;
+		const croppedHeight = bottom - top + 1;
+
+		// Crop the image
+		const croppedImage = image.crop({ x: left, y: top, width: croppedWidth, height: croppedHeight });
+
+		// Save the cropped image
+		await croppedImage.save(outputPath).catch((err)=>{ console.log(err); });
+		console.log(`Cropped image saved to ${outputPath}`);
+	}
 
 	console.log("[Realistic Weapons] Greenscreen Screenshot is enabled! This should only be enabled during setup!");
 
@@ -39,6 +115,15 @@ if(config.enable){
 			if (!fs.existsSync(savePath)) {
 				fs.mkdirSync(savePath);
 			}
+
+			if (!fs.existsSync(savePath+"/hd")) {
+				fs.mkdirSync(savePath+"/hd");
+			}
+
+			if (!fs.existsSync(savePath+"/square")) {
+				fs.mkdirSync(savePath+"/square");
+			}
+
 			exports['screenshot-basic'].requestClientScreenshot(
 				source,
 				{
@@ -47,6 +132,7 @@ if(config.enable){
 					quality: 1.0,
 				},
 				async (err, fileName) => {
+					const originalFileName=fileName;
 					let image = await imagejs.Image.load(fileName);
 					const coppedImage = image.crop({ x: image.width / 4.5, width: image.height });
 
@@ -67,7 +153,11 @@ if(config.enable){
 						}
 					}
 
-					image.save(fileName);
+					const path=beforeLast(fileName,"/");
+					const nameFile=afterLast(fileName,"/");
+
+					await trimTransparentBorders(originalFileName, path + "/hd/" + nameFile);
+					image.save(path+"/square/"+nameFile);
 				}
 			);
 		});
